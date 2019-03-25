fed0179545e17f1861608250c0d862657d41d638
gipark <gipark2001@naver.com>
2019-03-25 15:01:53 +0900

    summary added

--- linear_regression.py
+++ linear_regression.py
@@ -27,3 +27,18 @@ def gradientDescent(X, y, theta, alpha, num_iters):
         y_hat = np.dot(X, theta)
         theta = theta - alpha * (1.0/m) * np.dot(X.T, y_hat-y)
     return theta
+
+"""
+Mini-Batch Gradient Descent
+
+It is similar like SGD, it uses n samples instead of 1 at each iteration.
+Summary:
+
+In a summary, explained about the following topics in detail.
+
+Linear regression’s independent and dependent variables
+Ordinary Least Squares (OLS) method and Sum of Squared Errors (SSE) details
+Gradient descent for linear regression model and types gradient descent algorithms.
+
+https://towardsdatascience.com/linear-regression-simplified-ordinary-least-square-vs-gradient-descent-48145de2cf76
+"""
